95b725a30d882b89b041ea6bf68cfbb32dd240e2
Hemalatha <hemalathalekkala@gamil.com>
2022-11-03 21:50:20 +0530

    first commit

---
+++ es6/05_string_number_methods/main.js
@@ -0,0 +1,22 @@
+"use srict"
+
+let theString = 'Hello, my name is Brad and I love JavaScript';
+
+// stratsWith()
+// endsWith()
+// includes()
+
+console.log(theString.startsWith('Hello'));
+console.log(theString.endsWith('JavaScript'));
+console.log(theString.includes('love'));
+
+// Hex
+console.log(0xFF);
+// Binary
+console.log(0b101011);
+// Octal
+console.log(0o543);
+
+console.log(Number.isFinite(3));
+console.log(Number.isNaN('string'));
+console.log(Number.isInteger(2));
